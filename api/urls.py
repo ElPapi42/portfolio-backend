fd41f1577c0da089d028c2163c85f5a9f822ad15
ElPapi42 <whitman-2@hotmail.com>
2020-06-15 05:58:08 -0400

    Add basic contrib endpoint

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from api import views
+
+urlpatterns = [
+    path('contribs/', views.GithubContribs.as_view()),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class GithubContribs(View):
+    """Fetches and format github conrib history."""
+    def get(self, request):
+        return HttpResponse('List of Contribs')
